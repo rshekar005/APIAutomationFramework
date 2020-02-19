af33d3bf7a40d289fc4ff8c3ff3ac4062a53ce48
rshekar005 <rshekar005@gmail.com>
2020-02-19 08:31:20 +0530

    Added get place feature method in step definition

--- src/main/java/com/stepdefinition/AddPlaceAPI.java
+++ src/main/java/com/stepdefinition/AddPlaceAPI.java
@@ -11,6 +11,7 @@ import com.pojo.MapPojo;
 import com.resources.APINames;
 import com.resources.Payloads;
 import com.resources.Utils;
+import com.sun.xml.bind.v2.runtime.output.Pcdata;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -30,7 +31,7 @@ public class AddPlaceAPI extends Utils
 	RequestSpecification res;
 	Response response;
 	Payloads addplacepayload= new Payloads();
-	
+	String place_id;
 	//Static Data through pojo
 	/*@Given("Add place payload")
 	public void add_place_payload() throws IOException {
@@ -54,16 +55,19 @@ public class AddPlaceAPI extends Utils
 	}*/
 	
 	//Dynamic method which used enums to get required api name
-	@When("user calls {string} with post http request")
+	@When("user calls {string} with {string} http request")
 	//Here resource will come from feature file
-	public void user_calls_with_post_http_request(String resource) throws FileNotFoundException {
+	public void user_calls_with_post_http_request(String resource, String method) throws FileNotFoundException {
 		//valueOf method will take resource value and assign it to an oject
 		APINames apiname=APINames.valueOf(resource);
 		//From Enum object we are returning api name
 		System.out.println(apiname.getResourceName());
 	ResponseSpecification resp= new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
-	   response=res.when().post(apiname.getResourceName()).then().spec(resp).extract().response();
-	   System.out.println(response.asString());
+	if(method.equalsIgnoreCase("POST"))
+		 response =res.when().post(apiname.getResourceName());
+		else if(method.equalsIgnoreCase("GET"))
+			 response =res.when().get(apiname.getResourceName());
+	System.out.println(response.asString());
 	}
 
 	@Then("the api call is sucess with the status code with {int}")
@@ -73,8 +77,22 @@ public class AddPlaceAPI extends Utils
 
 	@Then("{string} in response is {string}")
 	public void in_response_is(String keyValue, String expectedValue) {
-	    String resp=response.asString();
-	    JsonPath json= new JsonPath(resp);
-	    Assert.assertEquals(expectedValue,json.get(keyValue).toString());
+		//Here we are printing expected value from Response matching with the feature file
+		Assert.assertEquals(expectedValue, getResponseValue(response, keyValue));
 	}
+	
+	@Then("verify the place_id created that maps to {string} using {string}")
+	public void verify_the_place_id_created_that_maps_to_name_using(String string,String resource) throws IOException {
+		 place_id=getResponseValue(response, "place_id");
+		 System.out.println("Place Id is "+place_id);
+		res=given().spec(requestSpecification()).queryParam("place_id", place_id);	
+		user_calls_with_post_http_request(resource, "GET");
+		String name=getResponseValue(response, "name");
+		System.out.println("Name is " +name+ "place is" +place_id);
+		//Assert.assertEquals(string, name);
+		
+	    
+	}
+	
+	
 }
