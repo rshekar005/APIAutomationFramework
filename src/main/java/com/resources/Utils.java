07c7fd90479d3f26dd3312d12f70d20314c9aeee
rshekar005 <rshekar005@gmail.com>
2020-02-19 08:30:26 +0530

    Added Common method to get required value from response based on key

--- src/main/java/com/resources/Utils.java
+++ src/main/java/com/resources/Utils.java
@@ -14,11 +14,15 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class Utils 
 {
     public static RequestSpecification req;
+    
+    //Method to send required query param, base uri, log filters for both request and response
 	public  RequestSpecification requestSpecification() throws IOException
 	{
 		if(req==null)
@@ -30,19 +34,27 @@ public class Utils
                 addFilter(RequestLoggingFilter.logRequestTo(log)).//It is used to log request
                 addFilter(ResponseLoggingFilter.logResponseTo(log)).//It is used to log response
                 setContentType(ContentType.JSON).build();
+		   System.out.println(req);
 		   return req;
 		}
 		return req;
 	}
     
+	//Method to get value from config file
 	public String getPropertyValue(String key) throws IOException
 	{
 		Properties prop= new Properties();
 		FileInputStream f= new FileInputStream(System.getProperty("user.dir")+"/src/main/java/com/resources/config.properties");
 		prop.load(f);
 		String value=prop.getProperty(key);
-		return value;
-		
-		
+		return value;	
+	}
+	
+	//Method to get required value from response
+	public String getResponseValue(Response response,String key)
+	{
+		String res=response.asString();
+		JsonPath js= new JsonPath(res);
+		return js.get(key).toString();
 	}
 }
