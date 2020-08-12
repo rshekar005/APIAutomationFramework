a212266a05715e46601acddb3dd7789c37e37d1f
rshekar005 <rshekar005@gmail.com>
2020-08-12 15:28:23 +0530

    Added Cucumopton plugin to generate report

--- src/main/java/com/testrunner/TestRunner.java
+++ src/main/java/com/testrunner/TestRunner.java
@@ -6,18 +6,18 @@ import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.TestNGCucumberRunner;
 
 @RunWith(Cucumber.class)
 @CucumberOptions
 (
 		features= "E:/Git/APIAutomationRahulShetty/src/main/java/com/feature",//Path of a feature
 		glue={"com/stepdefinition"},// Path of Step Definitions
-		//format= {"pretty","html:test-outout", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}, //to generate different types of reporting(output)
+		//format= {"pretty","html:test-output", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}, //to generate different types of reporting(output)
 		monochrome=true,//It will generate a console output in readable format.
 	   // strict=true,//It will fail execution if any pending steps are not there.(Example: If in step definition any test is missed then test execution fails)
 	    dryRun=false,
-	   tags={"@RegressionTest"}//It will run a scenario which is declared with @AddPlaceAPI in feature file
-)
+	   tags={"@SanityTest"},//It will run a scenario which is declared with @AddPlaceAPI in feature file
+		plugin = "json:target/cucumber-reports/CucumberTestReport.json")
 public class TestRunner {
-
 }
